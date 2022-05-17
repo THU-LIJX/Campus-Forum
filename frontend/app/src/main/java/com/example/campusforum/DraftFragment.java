002f44caae51800a03f7ce4b99523d1ab5ae49ab
Musky <thu.musky@gmail.com>
2022-05-17 20:57:30 +0800

    合并分支

--- frontend/app/src/main/java/com/example/campusforum/DraftFragment.java
+++ frontend/app/src/main/java/com/example/campusforum/DraftFragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
+/**   
  * A simple {@link Fragment} subclass.
  * Use the {@link DraftFragment#newInstance} factory method to
  * create an instance of this fragment.

74fc1badb178abbdfc7573bb7b44e41c0905d1f4
Musky <thu.musky@gmail.com>
2022-05-26 21:53:49 +0800

    搜索界面完善

--- frontend/app/src/main/java/com/example/campusforum/SearchActivity.java
+++ frontend/app/src/main/java/com/example/campusforum/SearchActivity.java
@@ -35,52 +35,42 @@ public class SearchActivity extends AppCompatActivity {
     private List<PostAdapter.Post> postList;
     private PostAdapter postAdapter;
     private LinearLayoutManager linearLayoutManager;
-    private int currentPage = -1;
-    private int pageSize = 5;
-    private String sortLiked = "DES";
-    private String sortTime = "DES";
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
         binding = ActivitySearchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         postList = new ArrayList<>();
         postAdapter = new PostAdapter(postList, this);
         linearLayoutManager = new LinearLayoutManager(this);
         binding.activitySearchRecyclerView.setAdapter(postAdapter);
         binding.activitySearchRecyclerView.setLayoutManager(linearLayoutManager);
 
-
         ArrayList<String> items = new ArrayList<>();
+        ArrayList<String> items_kind = new ArrayList<>();
         //! text、image、sound、video
         items.add("文字"); //text
         items.add("图像"); // image
         items.add("语音"); // sound
         items.add("视频"); // video
+        //! title, content
+        items_kind.add("动态标题");
+        items_kind.add("动态内容");
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.serach_list_item, items);
+        ArrayAdapter<String> adapter_kind = new ArrayAdapter<String>(this, R.layout.serach_list_item, items_kind);
         binding.searchSelect.setAdapter(adapter);
-        setContentView(binding.getRoot());
-
-        binding.activitySearchRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    int lastPosition = ((LinearLayoutManager) Objects.requireNonNull(recyclerView.getLayoutManager())).findLastVisibleItemPosition();
-                    if (lastPosition == postList.size() - 1) {
-                        getSearchResult();
-                    }
-                }
-            }
-        });
+        binding.searchSelectTitleOrContent.setAdapter(adapter_kind);
 
 
         binding.btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                postList.clear();
+                postAdapter.notifyDataSetChanged();
                 getSearchResult();
             }
         });
@@ -88,74 +78,86 @@ public class SearchActivity extends AppCompatActivity {
 
     private void getSearchResult() {
         String searchContent = Objects.requireNonNull(binding.searchKeyWord.getText()).toString();
-                String searchUserNameContent = binding.searchUserName.getText().toString();
-                String searchTypeContent = binding.searchSelect.getText().toString();
-                Log.d(TAG, "onClick: "+ searchTypeContent);
-                HashMap<String, String> data = new HashMap<>();
+        String searchUserNameContent = binding.searchUserName.getText().toString();
+        String searchTypeContent = binding.searchSelect.getText().toString();
+        String searchKeyWordType = binding.searchSelectTitleOrContent.getText().toString();
+        String searchTypeContent_Convert;
+        //! 发送四个包， type, username, content, title
+        HashMap<String, String> data = new HashMap<>();
+        data.put("user_name", searchUserNameContent);
+        switch (searchTypeContent) {
+            case "文字":
+                searchTypeContent_Convert = "text";
+                break;
+            case "图像":
+                searchTypeContent_Convert = "image";
+                break;
+            case "语音":
+                searchTypeContent_Convert = "voice";
+                break;
+            case "视频":
+                searchTypeContent_Convert = "video";
+                break;
+            default:
+                searchTypeContent_Convert = "";
+                break;
+        }
+        data.put("type", searchTypeContent_Convert);
+        switch (searchKeyWordType) {
+            case "动态标题":
+                data.put("title", searchContent);
+                data.put("content", "");
+                Log.d(TAG, "user_name:" + searchUserNameContent + ";type" + searchTypeContent_Convert + ";title:" + searchContent + ";content:" + "null");
+                break;
+            case "动态内容":
                 data.put("content", searchContent);
-                data.put("username", searchUserNameContent);
-                switch (searchTypeContent) {
-                    case "文字":
-                        data.put("type", "text");
-                        Log.d(TAG, "传入参数：" + searchContent + "," + searchUserNameContent + ",text");
-                        break;
-                    case "图像":
-                        data.put("type", "image");
-                        Log.d(TAG, "传入参数：" + searchContent + "," + searchUserNameContent + ",image");
-                        break;
-                    case "语音":
-                        data.put("type", "sound");
-                        Log.d(TAG, "传入参数：" + searchContent + "," + searchUserNameContent + ",？？？sound");
-                        break;
-                    case "视频":
-                        data.put("type", "video");
-                        Log.d(TAG, "传入参数：" + searchContent + "," + searchUserNameContent + ",video");
-                        break;
-                    default:
-                        data.put("type", "photo?");
-                        Log.d(TAG, "getSearchResult: 进入搜索界面");
-                        break;
-                }
+                data.put("title", "");
+                Log.d(TAG, "username:" + searchUserNameContent + ";type" + searchTypeContent_Convert + ";title:" + "null" + ";content:" + searchContent);
+                break;
+            default:
+                break;
 
-               HttpUtil.sendGetRequest("/api/user/search", data, new Callback() {
-                   @Override
-                   public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                       e.printStackTrace();
-                   }
+        }
 
-                   @Override
-                   public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                        try {
-                            String result = Objects.requireNonNull(response.body()).string();
-                            JSONObject jsonObject = new JSONObject(result);
-                            if (jsonObject.getString("message").equals("ok")) {
-                                JSONArray jsonArray= jsonObject.getJSONArray("blogs");
+        System.out.println("anchor");
+        System.out.println(data);
+
+       HttpUtil.sendGetRequest("/api/user/search", data, new Callback() {
+           @Override
+           public void onFailure(@NonNull Call call, @NonNull IOException e) {
+               e.printStackTrace();
+           }
+
+           @Override
+           public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                try {
+                    String result = Objects.requireNonNull(response.body()).string();
+                    JSONObject jsonObject = new JSONObject(result);
+                    if (jsonObject.getString("message").equals("ok")) {
+                        JSONArray jsonArray= jsonObject.getJSONArray("blogs");
 //                                Log.d(TAG, jsonArray.toString());
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        try {
-                                            System.out.println("Post Length" + jsonArray.length());
-                                            for (int i = 0; i < jsonArray.length(); i++) {
-                                                postList.add(new PostAdapter.Post(jsonArray.getJSONObject(i)));
-                                                postAdapter.notifyItemInserted(postList.size()-1);
-                                            }
-                                        } catch (JSONException e) {
-                                            e.printStackTrace();
-                                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    System.out.println("Post Length" + jsonArray.length());
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        postList.add(new PostAdapter.Post(jsonArray.getJSONObject(i)));
+                                        postAdapter.notifyItemInserted(postList.size()-1);
                                     }
-                                });
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                   }
-               });
+                        });
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+           }
+       });
     }
 
 
-//    private showMenu(View v, ) {
-
-//}
 }
 
